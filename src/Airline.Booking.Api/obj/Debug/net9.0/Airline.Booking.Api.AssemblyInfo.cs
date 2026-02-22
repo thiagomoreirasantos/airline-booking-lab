e1ba4d21c5639734162f41917b2c8db5e703544c
thiagomoreirasantos <tmoreiradossantos733@gmail.com>
2026-02-22 22:23:43 +0000

    fix: add .gitignore

--- src/Airline.Booking.Api/obj/Debug/net9.0/Airline.Booking.Api.AssemblyInfo.cs
+++ src/Airline.Booking.Api/obj/Debug/net9.0/Airline.Booking.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Airline.Booking.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a0f660bada18dbbf0e4309fc5ffa3b36fce5496")]
 [assembly: System.Reflection.AssemblyProductAttribute("Airline.Booking.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Airline.Booking.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
